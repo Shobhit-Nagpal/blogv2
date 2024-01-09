671ab94e88bac1d1c9798f840bb6da69bc059553
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-01-09 09:27:19 +0530

    Added base structure for frontend

---
+++ frontend/src/pages/AdminLoginPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AdminLoginPage() {
+
+}
+
+export default AdminLoginPage;
---
+++ frontend/src/pages/BlogDetailPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function BlogDetailPage() {
+
+}
+
+export default BlogDetailPage;
